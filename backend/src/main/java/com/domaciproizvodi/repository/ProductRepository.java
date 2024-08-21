108cd4ab549ede8f0543f4ecb882ad6b1f855775
Marko Markovic <markomarkoviccb@gmail.com>
2024-08-21 17:43:22 +0200

    add repositories

---
+++ backend/src/main/java/com/domaciproizvodi/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.domaciproizvodi.repository;
+
+import com.domaciproizvodi.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository  extends JpaRepository<Product, Long> {
+}
