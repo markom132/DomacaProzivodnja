320b7ebe7b4684e05780e6123872722245b38683
Marko Markovic <markomarkoviccb@gmail.com>
2024-08-21 14:43:46 +0200

    Initial commit

---
+++ domaciproizvodi/src/main/java/com/domaciproizvodi/DomaciProizvodiApplication.java
@@ -0,0 +1,13 @@
+package com.domaciproizvodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomaciProizvodiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomaciProizvodiApplication.class, args);
+	}
+
+}
