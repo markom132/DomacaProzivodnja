4cf68fd63b969745bd50dc87509ffa645bf17a29
Marko Markovic <markomarkoviccb@gmail.com>
2024-08-25 14:23:16 +0200

    implementing JWT authentication closes #41

---
+++ backend/src/main/java/com/domaciproizvodi/config/UserDetailsServiceConfig.java
@@ -0,0 +1,35 @@
+package com.domaciproizvodi.config;
+
+import com.domaciproizvodi.repository.UserRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+
+@Configuration
+public class UserDetailsServiceConfig implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserDetailsServiceConfig(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.domaciproizvodi.model.User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+
+}
