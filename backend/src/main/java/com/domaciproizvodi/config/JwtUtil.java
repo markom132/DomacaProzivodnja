10191a96c32e7631131f4a038a974eed9add1c0b
Marko Markovic <markomarkoviccb@gmail.com>
2024-08-29 22:06:27 +0200

    fix

--- backend/src/main/java/com/domaciproizvodi/config/JwtUtil.java
+++ backend/src/main/java/com/domaciproizvodi/config/JwtUtil.java
@@ -60,7 +60,6 @@ public class JwtUtil {
         .compact();
   }
 
-
   public Boolean validateToken(String token, UserDetails userDetails) {
     final String username = extractUsername(token);
     return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
