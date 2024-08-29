80cfc5f7c77949c8bde47b504f35925b721e028d
Marko Markovic <markomarkoviccb@gmail.com>
2024-08-29 22:34:17 +0200

    fix jococo

---
+++ backend/src/test/java/com/domaciproizvodi/DomaciProizvodiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.domaciproizvodi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DomaciProizvodiApplicationTests {
+
+  @Test
+  void dummyTest() {
+    assertTrue(true);
+
+  }
+}
