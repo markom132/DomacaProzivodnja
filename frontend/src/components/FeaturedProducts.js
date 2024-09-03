404d5845f3990101272a80510aef6c7399ba394c
Marko Markovic <markomarkoviccb@gmail.com>
2024-09-03 17:26:58 +0200

    fix and add slider

--- frontend/src/components/FeaturedProducts.js
+++ frontend/src/components/FeaturedProducts.js
@@ -4,32 +4,79 @@ import ProductCard from './ProductCard';
 import honeyImage from '../assets/med.jpg'; // Putanja do slike
 import cucumber from '../assets/cucumber.jpg';
 import milk from '../assets/milk.jpg';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+
 
 const FeaturedProducts = () => {
   const products = [
     { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
     { id: 2, image: cucumber, name: 'Krastavci', price: '$24.99' },
     { id: 3, image: milk, name: 'Mleko', price: '$8.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
+    { id: 1, image: honeyImage, name: 'Organski med', price: '$7.99' },
   ];
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <section style={styles.featuredSection}>
       <h2>Featured Products</h2>
-      <div style={styles.productList}>
-        {products.map(product => (
-          <Link
-            to={`/product/${product.id}`}
-            key={product.id}
-            style={styles.productLink}
-          >
-            <ProductCard
-              image={product.image}
-              name={product.name}
-              price={product.price}
-            />
-          </Link>
-        ))}
-      </div>
+      <Slider {...settings}>
+          {products.map(product => (
+            <Link
+              to={`/product/${product.id}`}
+              key={product.id}
+              style={styles.productLink}
+            >
+              <ProductCard
+                image={product.image}
+                name={product.name}
+                price={product.price}
+              />
+            </Link>
+          ))}
+      </Slider>
     </section>
   );
 };
@@ -39,14 +86,11 @@ const styles = {
     padding: '20px',
     textAlign: 'center',
   },
-  productList: {
-    display: 'flex',
-    justifyContent: 'center',
-    gap: '30px',
-  },
   productLink: {
     textDecoration: 'none',
     color: 'inherit',
+    flex: '1 1 250px',
+    maxWidth: '300px',
   },
 };
 
