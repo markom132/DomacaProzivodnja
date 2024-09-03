9e5033ef155987ff50af127d3189b86b4e6d6fc0
Marko Markovic <markomarkoviccb@gmail.com>
2024-09-03 14:19:46 +0200

    update closes #21

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import promoImage from '../assets/homePhoto.jpg'; // Putanja do slike
+import React, { useState, useEffect } from 'react';
+import promoImage from '../assets/homePhoto.jpg';
+import image1 from '../assets/broccoli.jpg';
+import image2 from '../assets/resized_image.png';
+import image3 from '../assets/med.jpg';
+import image4 from '../assets/milk.jpg';
 
 const HeroSection = () => {
+  const images = [promoImage, image1, image2, image3, image4];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, images.length);
+
   return (
-    <section style={{ ...styles.hero, backgroundImage: `url(${promoImage})` }}>
+    <section style={{...styles.hero, backgroundImage: `url(${images[currentIndex]})`}}>
       <div style={styles.promoText}>Specijalna ponuda domacih proizvoda!</div>
     </section>
   );
@@ -17,17 +32,20 @@ const styles = {
     alignItems: 'center',
     padding: '0 20px',
     textAlign: 'center',
+    backgroundSize: 'contain', // Ili 'cover' u zavisnosti od efekta koji želite
     backgroundSize: 'cover',
+    backgroundPosition: 'center', // Centriranje slike u okviru sekcije
     backgroundPosition: 'center',
     color: '#fff',
     borderRadius: '20px',
     marginBottom: '20px',
     overflow: 'hidden',
+    transition: 'background-image 1s ease-in-out',
   },
   promoText: {
     fontSize: '2em',
     fontWeight: 'bold',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Prozirna pozadina za tekst
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     padding: '20px',
     borderRadius: '10px',
     display: 'inline-block',
