39f753498ca40140ac320c5e69843803bc907bf6
Marko Markovic <markomarkoviccb@gmail.com>
2024-09-03 14:24:03 +0200

    fix

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import promoImage from '../assets/homePhoto.jpg';
 import image1 from '../assets/broccoli.jpg';
-import image2 from '../assets/resized_image.png';
+import image2 from '../assets/cucumber.jpg';
 import image3 from '../assets/med.jpg';
 import image4 from '../assets/milk.jpg';
 
@@ -32,9 +32,7 @@ const styles = {
     alignItems: 'center',
     padding: '0 20px',
     textAlign: 'center',
-    backgroundSize: 'contain', // Ili 'cover' u zavisnosti od efekta koji želite
     backgroundSize: 'cover',
-    backgroundPosition: 'center', // Centriranje slike u okviru sekcije
     backgroundPosition: 'center',
     color: '#fff',
     borderRadius: '20px',
