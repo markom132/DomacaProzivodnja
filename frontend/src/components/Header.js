2552de96acb807f287cddefa6c6bcc6701f327bb
Marko Markovic <markomarkoviccb@gmail.com>
2024-09-06 13:37:03 +0200

    fix

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,7 +14,6 @@ const Header = () => {
         <ul style={styles.navList}>
           <li style={styles.navItemContainer}>
             <NavLink
-              exact
               to="/"
               style={({ isActive }) => ({
                 ...styles.navItem,
