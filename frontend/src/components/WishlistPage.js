16d687da4090453674a39ae3004c998b5d9e6755
Marko Markovic <markomarkoviccb@gmail.com>
2024-09-07 20:53:28 +0200

    update

--- frontend/src/components/WishlistPage.js
+++ frontend/src/components/WishlistPage.js
@@ -1,24 +1,13 @@
 import React from 'react';
+import './WishlistPage.css';
 
 const WishlistPage = () => {
   return (
-    <div style={styles.pageContainer}>
-      <h2 style={styles.pageTitle}>Wishlist</h2>
+    <div className='pageContainer'>
+      <h2 className='pageTitle'>Wishlist</h2>
       <p>Your wishlist is currently empty.</p>
-      {/* Ovde ćeš prikazivati artikle na wishlisti kada budu dodati */}
     </div>
   );
-};
-
-const styles = {
-  pageContainer: {
-    padding: '20px',
-    textAlign: 'center',
-  },
-  pageTitle: {
-    fontSize: '2em',
-    marginBottom: '20px',
-  },
 };
 
 export default WishlistPage;
